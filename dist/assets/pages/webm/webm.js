f5125966f2ed1280c468e17e9a570fa74d8841e0
Dmitry <dmitry@cojam.ru>
2020-05-31 18:08:01 +0500

    начал рерайт webm-приложения

--- dist/assets/pages/webm/webm.js
+++ dist/assets/pages/webm/webm.js
@@ -1,94 +1,149 @@
 'use strict';
 
-$make.rnd = e => Math.floor(Math.random() * e);
+document.addEventListener('DOMContentLoaded', () => {
+    const chooseBox = $make.qs('.choose');
 
-var domain,
-    board,
-    thread,
-    video = $make.qs('video'),
-    reloadBtn = $make.qs('.load'),
-    nextBtn = $make.qs('.next'),
-    form = $make.qs('form'),
-    submitBtn = form.querySelector('input[type="submit"]');
+    const form = $make.qsf('.choose_form', chooseBox);
+    const formInputs = $make.qsf('input, button', form, ['a']);
+    const bookmark = $make.qsf('.choose_bookmark a', chooseBox);
 
-function get(e, t, o) {
-    fetch('https://cors-anywhere.herokuapp.com/https://' + e + '/' + t + '/res/' + o + '.json', {
-        cache: 'no-store',
-    }).then(function (n) {
-        n.json().then(function (n) {
-            var a = n.threads[0].posts,
-                c = [];
+    const video = $make.qs('.video');
 
-            function r() {
-                video.setAttribute('src', 'https://' + e + '/' + t + '/src/' + o + '/' + c[$make.rnd(c.length - 1)]);
+    const controls = $make.qs('.controls');
+    const controlsClose = $make.qsf('.controls__close', controls);
+    const controlsReload = $make.qsf('.controls__reload', controls);
+    const controlsNext = $make.qsf('.controls__next', controls);
+
+    const formInputsAction = action => {
+        formInputs.forEach(input => {
+            switch (action) {
+                case 'block':
+                    input.disabled = true;
+                    break;
+                case 'unlock':
+                    input.disabled = false;
+                    break;
+                default:
             }
-
-            a.forEach(function (e) {
-                for (var t = 0; t < e.files.length; t++) {
-                    let fileName = e.files[t].name;
-
-                    if (
-                        fileName.endsWith('webm') ||
-                        fileName.endsWith('mp4') ||
-                        fileName.endsWith('mkv') ||
-                        fileName.endsWith('ogv') ||
-                        fileName.endsWith('ogg') ||
-                        fileName.endsWith('avi') ||
-                        fileName.endsWith('3gp')
-                    ) {
-                        c[c.length] = e.files[t].name;
-                    }
-                }
-            });
-
-            r();
-
-            video.onended = function () {
-                r();
-            };
-            video.onerror = function () {
-                r();
-            };
-            nextBtn.onclick = function () {
-                r();
-            };
         });
-    });
-}
+    };
 
-!(function () {
-    var e = $check.get('domain'),
-        t = $check.get('board'),
-        o = $check.get('thread');
+    const random = num => Math.floor(Math.random() * num);
 
-    if (!o) return void document.body.classList.add('no-choose');
+    const threadURLData = url => {
+        const threadURL = new URL(url);
 
-    switch (((thread = o), (board = t ? t : 'b'), e)) {
-        case 'pm':
-            domain = '2ch.pm';
-            break;
-        case 'hk':
-        default:
-            domain = '2ch.hk';
-    }
+        const threadURLSplited = threadURL.pathname.split('/');
 
-    get(domain, board, thread);
-})()(function () {
-    form.onsubmit = function (e) {
+        return {
+            host: threadURL.hostname,
+            board: threadURLSplited[1],
+            thread_num: threadURLSplited[3].replace('.html', ''),
+        };
+    };
+
+    const setRandomVideo = (videos, threadData) => {
+        if (!videos && !threadData) {
+            videos = JSON.parse(sessionStorage.getItem('e965:webm:videos'));
+            threadData = JSON.parse(sessionStorage.getItem('e965:webm:threadData'));
+        } else {
+            sessionStorage.setItem('e965:webm:videos', JSON.stringify(videos));
+            sessionStorage.setItem('e965:webm:threadData', JSON.stringify(threadData));
+        }
+
+        video.setAttribute(
+            'src',
+            'https://' + threadData.host + '/' + threadData.board + '/src/' + threadData.thread_num + '/' + videos[random(videos.length - 1)]
+        );
+    };
+
+    const showVideo = (videos, threadData) => {
+        chooseBox.hidden = true;
+
+        video.hidden = false;
+        controls.hidden = false;
+
+        setRandomVideo(videos, threadData);
+    };
+
+    const hideVideo = () => {
+        chooseBox.hidden = false;
+
+        video.hidden = true;
+        controls.hidden = true;
+    };
+
+    const APIRequest = threadData => {
+        const proxy = 'https://cors-anywhere.herokuapp.com';
+
+        formInputsAction('block');
+
+        fetch(`${proxy}/https://${threadData.host}/${threadData.board}/res/${threadData.thread_num}.json`, { cache: 'no-store' })
+            .then(r => r.json())
+            .then(data => {
+                const posts = data.threads[0].posts;
+                const videos = [];
+
+                posts.forEach(post => {
+                    const postFiles = post.files;
+
+                    for (let i = 0; i < postFiles.length; i++) {
+                        const fileName = postFiles[i].name;
+
+                        if (
+                            fileName.endsWith('webm') ||
+                            fileName.endsWith('mp4') ||
+                            fileName.endsWith('mkv') ||
+                            fileName.endsWith('ogv') ||
+                            fileName.endsWith('ogg') ||
+                            fileName.endsWith('avi') ||
+                            fileName.endsWith('3gp')
+                        ) {
+                            videos.push(fileName);
+                        }
+                    }
+                });
+
+                if (videos.length !== 0) {
+                    showVideo(videos, threadData);
+                } else {
+                    alert('В треде нет ни одного видео');
+                }
+            })
+            .catch(e => {
+                alert('Возникла какая-то ошибка' + e);
+            })
+            .finally(() => {
+                formInputsAction('unlock');
+            });
+    };
+
+    form.addEventListener('submit', e => {
         e.preventDefault();
 
-        var t = form.querySelector('input[name="domain"]').value,
-            o = form.querySelector('input[name="board"]').value,
-            n = form.querySelector('input[name="thread"]').value;
+        const formData = new FormData(e.target);
+        const data = threadURLData(formData.get('thread_url'));
 
-        thread = n;
-        board = o ? o : 'b';
-        (domain = t ? t.replace('https:', '').replace('http:', '').replace('//', '').replace('/', '') : '2ch.hk'),
-            document.body.classList.remove('no-choose');
-
-        get(domain, board, thread);
-    };
-})(),
-    (reloadBtn.onclick = function () {
-        get(domain, board, thread);
+        APIRequest(data);
     });
+
+    void (() => {
+        let threadURL = '';
+
+        try {
+            threadURL = new URL($check.get('thread_url'));
+        } catch (e) {}
+
+        if (threadURL) {
+            formInputs[0].value = threadURL;
+            formInputs[1].click();
+        }
+    })();
+
+    bookmark.href = `javascript:`;
+
+    video.onended = () => setRandomVideo();
+    video.onerror = () => setRandomVideo();
+
+    controlsNext.onClick = () => setRandomVideo();
+});
